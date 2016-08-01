3c76b1e29bdfdd52daed57c237f1bd2554d3ab2c
dory <example@example>
2016-08-01 10:30:54 +0900

    add test cpp

---
+++ cpp/src/template_test2.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+
+template< unsigned n>
+struct factorial {
+	static const unsigned value = n * factorial<n-1>::value;
+};
+
+template<>
+struct factorial<0> {
+	static const unsigned value = 1;
+};
+
+int main(){
+	const struct factorial<10> n;
+	std::cout << n.value << std::endl;	
+	
+	const unsigned int gift = 20;
+	const struct factorial<gift> n2;
+	std::cout << n2.value << std::endl;
+
+	return 0;
+}

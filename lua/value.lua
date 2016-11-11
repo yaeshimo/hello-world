5e893d24d2dde1095b78860bfe1c603334d42691
dorymint <sk.dorymint@gmail.com>
2016-11-11 20:07:26 +0900

    add cases  On branch master  Changes to be committed: 	new file:   value.lua

---
+++ lua/value.lua
@@ -0,0 +1,19 @@
+hoge = 10
+print("hoge\t=", hoge)
+
+fuga = 0.101
+print("fuga\t=", fuga)
+
+print(fuga==hoge)
+
+hoge, fuga = 20, 0.202 --muxing
+print(fuga, hoge)
+print(string.format("fuga=%d, hoge=%d", fuga, hoge))
+
+print "\n"
+print("+", hoge + fuga)
+print("-", hoge - fuga)
+print("*", hoge * fuga)
+print("/", hoge / fuga)
+print("%", hoge % fuga)
+print "\n"

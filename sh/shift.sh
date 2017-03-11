f18705e90ffce2617e31cc5e17cbf33df4335840
dorymint <sk.dorymint@gmail.com>
2017-03-11 12:04:24 +0900

    add sh shift test  Changes to be committed: 	modified:   shift.sh

--- sh/shift.sh
+++ sh/shift.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 set -eu
+echo '$*'"=$*"
+echo '$#'"=$#"
+echo "----- SHIFT TEST -----"
 for i in `seq 1 $#`; do
-  echo $1
+  echo "... $#"
+  echo '$*='"$*"
+  echo '$1='"$1"
   shift
 done
+echo "----- TEST END -----"
+echo '$*='"$*"
+echo '$#='"$#"
 # EOF

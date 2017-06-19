55786bd9f2e650dd7895ea8e8d9d00127df40b9b
kamisari <kamisari10yonaga@gmail.com>
2017-06-20 00:09:04 +0900

    add node  Changes to be committed: 	new file:   .tern-project 	renamed:    hello-world.js -> hello-world/hello-world.js 	renamed:    hello.js -> hello-world/hello.js 	renamed:    world.js -> hello-world/world.js 	new file:   variable/main.js

---
+++ nodejs/variable/main.js
@@ -0,0 +1,44 @@
+var v1 = 10; // function scope
+g1 = 1; // global
+let l1 = 100; // block scope
+
+console.log("definition: ", g1, v1, l1);
+console.log();
+
+function func() {
+  g2 = 2;
+  var v2 = 20;
+  let l2 = 200;
+  console.log("in function: ", g1, v1, l1);
+  console.log("in function: ", g2, v2, l2);
+  console.log();
+
+  {
+    let l2 = 200*2;// new
+    console.log("scope1:", g2, v2, l2);
+    console.log("l2*2: ", l2);// 400
+
+  }
+  console.log("exit scope2 l2: ", l2);// 200
+  console.log();
+
+  {
+    var v2 = 20*2;
+    console.log("scope2:", g2, v2, l2);
+    console.log("v2*2: ", v2);
+  }
+  console.log("exit scope2 v2: ", v2);
+  console.log();
+
+}
+func();
+
+console.log("after function g2: ", g2);
+console.log();
+
+try {
+  console.log(v2, l2); // definition error
+}
+catch (e) {
+  console.error("CATCH ERROR:\n", e);
+}

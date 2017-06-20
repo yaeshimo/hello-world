c2155a5ef616ee3769822e0927a2aab0da4192c7
kamisari <kamisari10yonaga@gmail.com>
2017-06-20 11:14:03 +0900

    add test  Changes to be committed: 	modified:   main-test.js

--- nodejs/test/main-test.js
+++ nodejs/test/main-test.js
@@ -7,9 +7,15 @@ function assert(i, exp, out) {
 
 function Testadd() {
   console.log("- Testadd");
+
+  //type input struct { arg1, arg2 int}
+  //tests := []struct{ exp int, in input}{{exp: 0, in: input{ arg1:1, arg2:2}},}
+  //for i, v := range tests {
+  //  assert(i, v.exp, main.add(v.in1,v.in2))
+  //}
+
   let caseCounter = 0;
-  //for{};
-  assert(caseCounter, 0, main.add(1,1));
+  assert(caseCounter, 0, main.add(1,1));// expected 0 but 2
 }
 
 Testadd();

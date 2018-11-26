a34c5efb913bbc3a5f68e225d1d2cf1b0b462d07
yaeshimo <hatsushimoyae@gmail.com>
2018-11-26 15:59:35 +0900

    add loop counter

--- sh/argv.sh
+++ sh/argv.sh
@@ -1,28 +1,52 @@
 #!/bin/sh
 set -eu
 
+counter=0
+
+cinit() {
+  counter=0
+}
+cadd() {
+  counter=$(expr ${counter} + 1)
+}
+cout() {
+  echo "${counter} loops"
+}
+
 main() {
   echo "-----\"\$*\"-----"
+  cinit
   for x in "$*"; do
     echo "$x"
+    cadd
   done
+  cout
 
   echo "-----\$*-----"
+  cinit
   for x in $*; do
+    cadd
     echo "$x"
   done
+  cout
 
   echo "-----\"\$@\"-----"
+  cinit
   for x in "$@"; do
+    cadd
     echo "$x"
   done
+  cout
 
   echo "-----\$@-----"
+  cinit
   for x in $@; do
+    cadd
     echo "$x"
   done
+  cout
 }
 
 [ "$#" -eq 0 ] &&
-  main arg1 arg2 arg3 arg4 arg5 ||
+  main arg1 arg2 "arg3 arg4 arg5" arg6 ||
   main "$@"
